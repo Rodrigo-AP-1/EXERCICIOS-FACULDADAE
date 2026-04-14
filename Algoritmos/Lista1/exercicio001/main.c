f1fd0c8362f06f33ae827ed5102516c807c066b7
Rodrigoha <vinicius.a.prates2@gmail.com>
2026-04-13 21:06:33 -0300

    feat: c exercise done

---
+++ Algoritmos/Lista1/exercicio001/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int b, a, area;
+
+    printf("qual a base do retangulo?:\n ");
+    scanf("%d", &b);
+    printf("qual a altura do retangulo?:\n ");
+    scanf("%d", &a);
+
+    area = b*a;
+
+    printf("area do retangulo: %d ", area);
+
+
+    
+}
