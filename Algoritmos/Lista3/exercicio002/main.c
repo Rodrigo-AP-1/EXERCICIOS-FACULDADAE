30741a01a9ec3a6a28d0352ddfdc16180fd23561
Rodrigoha <vinicius.a.prates2@gmail.com>
2026-04-16 12:00:57 -0300

    feat: c exercise done

---
+++ Algoritmos/Lista3/exercicio002/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+
+    printf("Digite os valores dos numeros: \n");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > b && b > c) {
+        printf("Ordem Crescente: %d, %d, %d", c, b, a);
+    } else if (b > a && a > c) {
+        printf("Ordem Crescente: %d, %d, %d", c, a, b);
+    } else if (c > a && a > b) {
+        printf("Ordem Crescente: %d, %d, %d", b, a, c);
+    } else if (a > c && c > b) {
+        printf("Ordem Crescente: %d, %d, %d", b, c, a);
+    } else if (b > c && c > a) {
+        printf("Ordem Crescente: %d, %d, %d", a, c, b);
+    } else {
+        printf("Ordem Crescente: %d, %d, %d", a, b, c);
+    }
+
+    return 0;
+}
